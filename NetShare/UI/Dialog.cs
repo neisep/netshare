3340637c7e84736aed60a86194b7ec19768b1c91
jimmie <jimmie@neisep.com>
2023-03-05 15:02:03 +0100

    fix(main): added main project in .net
    added main project in .net was going todo it in golang but i did not get it too work properly in my windows machine, so will do it later.

---
+++ NetShare/UI/Dialog.cs
@@ -0,0 +1,32 @@
+﻿using Infrastracture;
+using System;
+using System.Windows.Forms;
+
+namespace NetShare.UI
+{
+    public partial class Dialog : Form
+    {
+        public Object ResultObject { get; set; }
+        public Dialog()
+        {
+            InitializeComponent();            
+        }
+
+        private void Dialog_Load(object sender, EventArgs e)
+        {
+            foreach (var item in Controls)
+            {
+                if (typeof(UserControl) == item.GetType().BaseType)
+                {
+                    var userControl = (UserControl)item;
+                    Text = $"{userControl.Name} {Helper.ApplicationName}";
+
+                    this.Width = userControl.Width;
+                    this.Height = userControl.Height;
+                }
+            }
+
+            this.CenterToParent();
+        }
+    }
+}
